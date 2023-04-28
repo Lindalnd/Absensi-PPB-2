5be8e0af5961fc158fcb32fe07b4d2ff9f522dd5
Inytl <inytl.maula18@gmail.com>
2023-04-28 17:40:25 +0700

    layout login Mhs
    layout login mahasiswa

---
+++ app/src/main/java/com/lindainaya/absensippb/LoginMhsActivity.kt
@@ -0,0 +1,11 @@
+package com.lindainaya.absensippb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginMhsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_mhs)
+    }
+}  
