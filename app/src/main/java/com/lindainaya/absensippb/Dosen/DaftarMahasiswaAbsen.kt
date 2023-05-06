ceda8b1416a8b9a4f011c60ac02810c9e8ea48d5
Linda Hasanah <98062453+Lindalnd@users.noreply.github.com>
2023-05-06 20:48:43 +0700

    Desain Layout Page Dosen
    menambahkan layout untuk profil dosen dan daftar mahasiswa hadir, izin dan absen

---
+++ app/src/main/java/com/lindainaya/absensippb/Dosen/DaftarMahasiswaAbsen.kt
@@ -0,0 +1,12 @@
+package com.lindainaya.absensippb.Dosen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lindainaya.absensippb.R
+
+class DaftarMahasiswaAbsen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_mahasiswa_absen)
+    }
+}
